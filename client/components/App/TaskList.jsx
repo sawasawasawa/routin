b9f347e600929cd7abbdd1f89fc331f5f0401738
sawasawasawa <msawka@thebrain.pro>
2016-05-05 18:06:07 +0200

    added bg img

--- client/components/App/TaskList.jsx
+++ client/components/App/TaskList.jsx
@@ -31,21 +31,23 @@ TaskList = React.createClass({
             }, {sort: {createdAt: 1}}).fetch()
         }
     },
-    
-    
+
 
     renderTable(taskType){
         // console.log('PINGWIN: this.data[taskType]', this.data[taskType]);
         return this.data[taskType].map((task) => {
-            switch(taskType) {
+            switch (taskType) {
                 case 'dueTasks':
-                    return <TaskRow type={taskType} key={task._id} keyId={task._id} task={task} subtasks={task.subtasks}/>;
+                    return <TaskRow type={taskType} key={task._id} keyId={task._id} task={task}
+                                    subtasks={task.subtasks}/>;
                     break;
                 case 'mit':
-                    return <MITRow type={taskType} key={task._id} keyId={task._id} task={task} subtasks={task.subtasks}/>;
+                    return <MITRow type={taskType} key={task._id} keyId={task._id} task={task}
+                                   subtasks={task.subtasks}/>;
                     break;
                 case 'habits':
-                    return <HabitRow type={taskType} key={task._id} keyId={task._id} task={task} subtasks={task.subtasks}/>;
+                    return <HabitRow type={taskType} key={task._id} keyId={task._id} task={task}
+                                     subtasks={task.subtasks}/>;
                     break;
                 case 'questionnaire':
                     return <QuestionnaireRow type={taskType} key={task._id} keyId={task._id} task={task}/>;
@@ -59,10 +61,11 @@ TaskList = React.createClass({
         this.copyTasksFromCategory('task');
         this.copyTasksFromCategory('mit');
         this.copyTasksFromCategory('habit');
+        this.copyTasksFromCategory('questionnaire');
     },
 
     copyTasksFromCategory(category){
-        const _checkedQuery = category == 'habit' ? {$exists: true} : false;
+        const _checkedQuery = category == 'habit' || category == 'questionnaire' ? {$exists: true} : false;
         var _tasksToCopy = Tasks.find({
             cat: category,
             checked: _checkedQuery,
@@ -73,7 +76,7 @@ TaskList = React.createClass({
         _tasksToCopy.forEach(function (myDoc) {
             var copy = myDoc;
             var _streak_arr = [];
-            if (category==='habit'){
+            if (category === 'habit') {
                 _streak_arr = copy.streak_arr;
                 _streak_arr.push(copy.checked);
                 _streak_arr.shift();
@@ -127,52 +130,55 @@ TaskList = React.createClass({
 
     render() {
         return (
-            <div>
-                <div className="container-fluid main-container main-content">
-                    <div className="centered">
-                        <button type="button" className="btn btn-primary btn-lg center" onClick={this.copyUnchecked}>
-                            Copy unchecked tasks
-                        </button>
+            <div className="taskList">
+                <div className="taskList-inner">
+                    <div className="container-fluid main-container main-content">
+                        <div className="centered">
+                            <button type="button" className="btn btn-primary btn-lg center"
+                                    onClick={this.copyUnchecked}>
+                                Copy unchecked tasks
+                            </button>
+                        </div>
                     </div>
-                </div>
 
-                <Challenge30 />
+                    <Challenge30 />
 
-                <div>
-                    <h3>Habits</h3>
-                    {this.data.habits.length <7 ? <HabitInput /> : null }
-                    <table className="table habit-table">
-                        <tbody>
-                        {this.renderTable('habits')}
-                        </tbody>
-                    </table>
-                </div>
-                <div>
-                    <h3>Most Important Tasks</h3>
-                    {this.data.mit.length <3 ? <MITInput /> : null }
-                    <table className="table mit-table">
-                        <tbody>
-                        {this.renderTable('mit')}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="table-responsive">
-                    <h3>Tasks</h3>
-                    <TaskInput />
-                    <table className="table task-table">
-                        <tbody>
-                        {this.renderTable('dueTasks')}
-                        </tbody>
-                    </table>
-                </div>
-                <div className="table-responsive">
-                    <h3>Daily Questionnaire</h3>
-                    <QuestionnaireInput />
-                    <table className="table questionnaire-table">
-                        <tbody>
-                        {this.renderTable('questionnaire')}
-                        </tbody>
-                    </table>
+                    <div>
+                        <h3>Habits</h3>
+                        {this.data.habits.length <7 ? <HabitInput /> : null }
+                        <table className="table habit-table">
+                            <tbody>
+                            {this.renderTable('habits')}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div>
+                        <h3>Most Important Tasks</h3>
+                        {this.data.mit.length <3 ? <MITInput /> : null }
+                        <table className="table mit-table">
+                            <tbody>
+                            {this.renderTable('mit')}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="table-responsive">
+                        <h3>Tasks</h3>
+                        <TaskInput />
+                        <table className="table task-table">
+                            <tbody>
+                            {this.renderTable('dueTasks')}
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="table-responsive">
+                        <h3>Daily Questionnaire</h3>
+                        <QuestionnaireInput />
+                        <table className="table questionnaire-table">
+                            <tbody>
+                            {this.renderTable('questionnaire')}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
 
